7a9f46dddf2a4f1f394ae20ea8145554725bca1a
susangormley11 <73467211+susangormley11@users.noreply.github.com>
2021-01-06 11:10:23 +0000

    added link to github

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ export default function App() {
   return (
     <div className="App">
      Weather App
+     <p>
+       Created by Susan Gormley and open-sourced on <a href="https://github.com/susangormley11/ReactWeatherApp1" target="_blank">GitHub</a>
+     </p>
     </div>
   );
 }
